4af91be217067c0b2a9d72eafe70330c729dcc43
carterbanks <carterbryantbanks@gmail.com>
2018-05-30 22:12:53 -0500

    New Logo

--- app/data/friends.js
+++ app/data/friends.js
@@ -9,36 +9,36 @@ var friendsArray = [
     {
         name: "Clarence",
         photo:
-            "https://cdn.pixabay.com/photo/2018/05/04/16/50/cat-3374422_1280.jpg",
+            "../public/images/clarence.jpg",
         scores: ["5", "1", "4", "4", "5", "1", "2", "5", "4", "1"]
     },
     {
         name: "Gob",
         photo:
-            "https://cdn.pixabay.com/photo/2016/11/18/19/54/animal-1836651_1280.jpg",
+            "../public/images/gob.jpg",
         scores: ["4", "2", "5", "1", "3", "2", "2", "1", "3", "2"]
     },
     {
         name: "Patrice",
-        photo: "http://1.bp.blogspot.com/-oueclaCFYgM/TbwxDP3l_HI/AAAAAAAAKbE/z1f3eO2xozo/s640/manul+cat+palluss+cat+pallus+1.jpg",
+        photo: "../public/images/patrice.jpg",
         scores: ["5", "2", "2", "2", "4", "1", "3", "2", "5", "5"]
     },
     {
         name: "Estaban",
         photo:
-            "https://cdn.pixabay.com/photo/2018/04/29/16/28/animal-3360397_1280.jpg",
+            "../public/images/estaban.jpg",
         scores: ["3", "3", "4", "2", "2", "1", "3", "2", "2", "3"]
     },
     {
         name: "Grant",
         photo:
-            "https://cdn.pixabay.com/photo/2018/03/22/23/21/cute-3252251_1280.jpg",
+            "../public/images/grant.jpg",
         scores: ["4", "3", "4", "1", "5", "2", "5", "3", "1", "4"]
     },
     {
         name: "Francine",
         photo:
-            "https://cdn.pixabay.com/photo/2018/01/05/13/00/cat-3062885_1280.jpg",
+            "../public/images/francine.jpg",
         scores: ["4", "4", "2", "3", "2", "2", "3", "2", "4", "5"]
     },
 ];
