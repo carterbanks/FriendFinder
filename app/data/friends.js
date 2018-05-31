0f6272671bf26ea02bb9e0703d63896209927f0a
carterbanks <carterbryantbanks@gmail.com>
2018-05-30 22:18:55 -0500

    Pics Fixed

--- app/data/friends.js
+++ app/data/friends.js
@@ -9,36 +9,36 @@ var friendsArray = [
     {
         name: "Clarence",
         photo:
-            "../public/images/clarence.jpg",
+            "https://github.com/carterbanks/FriendFinder/blob/master/app/public/images/clarence.jpg?raw=true",
         scores: ["5", "1", "4", "4", "5", "1", "2", "5", "4", "1"]
     },
     {
         name: "Gob",
         photo:
-            "../public/images/gob.jpg",
+            "https://github.com/carterbanks/FriendFinder/blob/master/app/public/images/gob.jpg?raw=true",
         scores: ["4", "2", "5", "1", "3", "2", "2", "1", "3", "2"]
     },
     {
         name: "Patrice",
-        photo: "../public/images/patrice.jpg",
+        photo: "https://github.com/carterbanks/FriendFinder/blob/master/app/public/images/patrice.jpg?raw=true",
         scores: ["5", "2", "2", "2", "4", "1", "3", "2", "5", "5"]
     },
     {
         name: "Estaban",
         photo:
-            "../public/images/estaban.jpg",
+            "https://github.com/carterbanks/FriendFinder/blob/master/app/public/images/estaban.jpg?raw=true",
         scores: ["3", "3", "4", "2", "2", "1", "3", "2", "2", "3"]
     },
     {
         name: "Grant",
         photo:
-            "../public/images/grant.jpg",
+            "https://github.com/carterbanks/FriendFinder/blob/master/app/public/images/grant.jpg?raw=true",
         scores: ["4", "3", "4", "1", "5", "2", "5", "3", "1", "4"]
     },
     {
         name: "Francine",
         photo:
-            "../public/images/francine.jpg",
+            "https://github.com/carterbanks/FriendFinder/blob/master/app/public/images/francine.jpg?raw=true",
         scores: ["4", "4", "2", "3", "2", "2", "3", "2", "4", "5"]
     },
 ];
